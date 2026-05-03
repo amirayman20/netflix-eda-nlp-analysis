f452ae596caeccbdffd2ff444d2d185dce1bee29
Amir Ayman <amirayman20@gmail.com>
2026-05-03 16:11:30 +0300

    Add global configuration for colors and stopwords

--- scripts/config.py
+++ scripts/config.py
@@ -1,1 +1,16 @@
+"""
+Global configuration:
+- colors
+- stopwords
+"""
+
+COLORS = {
+    'netflix_red': '#E50914',
+    'background': '#111111'
+}
+
+STOPWORDS = {
+    'series', 'movie', 'show', 'film', 'netflix',
+    'one', 'life', 'story', 'based', 'season'
+}
 
